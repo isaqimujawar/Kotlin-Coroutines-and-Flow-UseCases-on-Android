565ac00d1691ec28579793eae1850e58485c4faf
M Isaq <maddy829019@gmail.com>
2024-02-12 16:47:25 +0530

    Section 8: Performing the calculation in a coroutine by default on the main thread

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
@@ -1,10 +1,40 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase10
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import kotlinx.coroutines.launch
+import java.math.BigInteger
+import kotlin.system.measureTimeMillis
 
 class CalculationInBackgroundViewModel : BaseViewModel<UiState>() {
 
     fun performCalculation(factorialOf: Int) {
+        uiState.value = UiState.Loading
 
+        // Performing the calculation in a coroutine is by default on the main thread
+        viewModelScope.launch {
+
+            var result = BigInteger.ONE
+            val computationDuration = measureTimeMillis {
+                result = calculateFactorialOf(factorialOf)
+            }
+
+            var resultString = ""
+            val stringConversionDuration = measureTimeMillis {
+               resultString = result.toString()
+            }
+
+            uiState.value = UiState.Success(resultString, computationDuration, stringConversionDuration)
+        }
+    }
+
+    private fun calculateFactorialOf(number: Int): BigInteger {
+        var factorial = BigInteger.ONE
+
+        for (i in 1..number) {
+            factorial = factorial.multiply(BigInteger.valueOf(i.toLong()))
+        }
+
+        return factorial
     }
 }
