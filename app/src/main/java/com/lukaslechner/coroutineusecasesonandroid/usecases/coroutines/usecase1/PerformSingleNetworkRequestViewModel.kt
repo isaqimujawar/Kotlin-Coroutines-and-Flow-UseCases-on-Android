95e92e9a4c45662ea2c52914259e99f2fc417ec6
M Isaq <maddy829019@gmail.com>
2024-02-18 01:18:03 +0530

    Section 9 - viewModelScope

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/PerformSingleNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/PerformSingleNetworkRequestViewModel.kt
@@ -3,6 +3,8 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -13,7 +15,8 @@ class PerformSingleNetworkRequestViewModel(
     fun performSingleNetworkRequest() {
         uiState.value = UiState.Loading
 
-        viewModelScope.launch {
+        val job = viewModelScope.launch(Dispatchers.Main) {
+            Timber.tag("viewModelScope").d("I'm the first statement in the coroutine")
             try {
                 val recentAndroidVersions = mockApi.getRecentAndroidVersions()
                 uiState.value = UiState.Success(recentAndroidVersions)
@@ -21,6 +24,13 @@ class PerformSingleNetworkRequestViewModel(
                 Timber.e(exception)
                 uiState.value = UiState.Error("Network request failed!")
             }
+        }
+
+        Timber.tag("viewModelScope").d("I'm the first statement after launching the coroutine")
+
+        job.invokeOnCompletion { throwable ->
+            if (throwable is CancellationException)
+                Timber.tag("viewModelScope").d("Coroutine was cancelled!")
         }
     }
 }
