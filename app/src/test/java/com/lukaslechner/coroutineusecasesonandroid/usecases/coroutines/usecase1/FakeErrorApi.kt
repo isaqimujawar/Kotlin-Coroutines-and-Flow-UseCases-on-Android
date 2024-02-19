83d94c9a3c903bdc4c940cffd09c6ec3a4fb42aa
M Isaq <maddy829019@gmail.com>
2024-02-19 13:25:27 +0530

    Section 12 Testing Coroutines - Happy Path

---
+++ app/src/test/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/FakeErrorApi.kt
@@ -0,0 +1,27 @@
+package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
+
+import com.lukaslechner.coroutineusecasesonandroid.mock.AndroidVersion
+import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import com.lukaslechner.coroutineusecasesonandroid.mock.VersionFeatures
+import com.lukaslechner.coroutineusecasesonandroid.utils.EndpointShouldNotBeCalledException
+import okhttp3.MediaType
+import okhttp3.ResponseBody
+import org.mockito.internal.matchers.And
+import retrofit2.HttpException
+import retrofit2.Response
+
+class FakeErrorApi : MockApi {
+    override suspend fun getRecentAndroidVersions(): List<AndroidVersion> {
+        throw HttpException(
+            Response.error<List<AndroidVersion>>(
+                500,
+                ResponseBody.create(MediaType.parse("application/json"), "")
+            )
+        )
+    }
+
+    override suspend fun getAndroidVersionFeatures(apiLevel: Int): VersionFeatures {
+        throw EndpointShouldNotBeCalledException()
+    }
+
+}
--- app/src/test/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/PerformSingleNetworkRequestViewModelTest.kt
+++ app/src/test/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/PerformSingleNetworkRequestViewModelTest.kt
@@ -1,22 +1,30 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.lukaslechner.coroutineusecasesonandroid.mock.AndroidVersion
+import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
 import com.lukaslechner.coroutineusecasesonandroid.mock.mockAndroidVersions
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
+import okhttp3.MediaType
+import okhttp3.ResponseBody
 import org.junit.After
-import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
+import retrofit2.HttpException
+import retrofit2.Response
 
+@ExperimentalCoroutinesApi
 class PerformSingleNetworkRequestViewModelTest {
 
     @Before
@@ -32,7 +40,14 @@ class PerformSingleNetworkRequestViewModelTest {
 
     private val receivedUiStates = mutableListOf<UiState>()
 
-    @OptIn(ExperimentalCoroutinesApi::class)
+    private fun observeViewModel(viewModel: PerformSingleNetworkRequestViewModel) {
+        viewModel.uiState().observeForever { uiState ->
+            if (uiState != null)
+                receivedUiStates.add(uiState)
+        }
+    }
+
+
     @Test
     fun `should return Success when network request is successful`() = runTest {
 
@@ -49,7 +64,7 @@ class PerformSingleNetworkRequestViewModelTest {
         viewModel.performSingleNetworkRequest()
 
         // Assert
-        Assert.assertEquals(
+        assertEquals(
             listOf(UiState.Loading, UiState.Success(mockAndroidVersions)),
             receivedUiStates
             )
@@ -57,10 +72,33 @@ class PerformSingleNetworkRequestViewModelTest {
         Dispatchers.resetMain()
     }
 
-    private fun observeViewModel(viewModel: PerformSingleNetworkRequestViewModel) {
-        viewModel.uiState().observeForever { uiState ->
-            if (uiState != null)
-                receivedUiStates.add(uiState)
+    @Test
+    fun `should return Success when network request is successful using mockk`() = runTest {
+        // Arrange
+
+        val dispatcher = UnconfinedTestDispatcher()
+        Dispatchers.setMain(dispatcher)
+
+        val mockApi = mockk<MockApi>()
+        coEvery { mockApi.getRecentAndroidVersions() } returns mockAndroidVersions
+
+        val viewModel = PerformSingleNetworkRequestViewModel(mockApi)
+        observeViewModel(viewModel)
+
+        // Act
+        viewModel.performSingleNetworkRequest()
+
+        // Assert
+        assertEquals(
+            listOf(UiState.Loading, UiState.Success(mockAndroidVersions)),
+            receivedUiStates
+        )
+
+        // verify
+        coVerify(exactly = 1) {
+            mockApi.getRecentAndroidVersions()
         }
+
+        Dispatchers.resetMain()
     }
 }
