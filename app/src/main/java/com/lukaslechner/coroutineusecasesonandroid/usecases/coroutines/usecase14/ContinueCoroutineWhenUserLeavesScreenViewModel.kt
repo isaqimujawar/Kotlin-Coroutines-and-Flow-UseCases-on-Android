81465dfcdfd9bb04eae3c9866b147faa2e3180a0
M Isaq <maddy829019@gmail.com>
2024-02-22 14:27:38 +0530

    Section 9 Completed - Continue Coroutine execution when the user leaves the screen

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase14/ContinueCoroutineWhenUserLeavesScreenViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase14/ContinueCoroutineWhenUserLeavesScreenViewModel.kt
@@ -13,20 +13,29 @@ class ContinueCoroutineWhenUserLeavesScreenViewModel(
     // https://medium.com/androiddevelopers/coroutines-patterns-for-work-that-shouldnt-be-cancelled-e26c40f142ad
 
     fun loadData() {
+        // Step 1 - Show Loading Indicator for LoadFromDb
         uiState.value = UiState.Loading.LoadFromDb
 
+        // Step 2 - Launch a New Coroutine in the viewModelScope
         viewModelScope.launch {
+            // Step 3 - Get Data from the Repository
             val localVersions = repository.getLocalAndroidVersions()
+
+            // Step 4 - Check if Data returned from the Repository is Empty
+            // Step 4a - if Data isNotEmpty, then show UiState.Success(localData)
             if (localVersions.isNotEmpty()) {
                 uiState.value =
                     UiState.Success(DataSource.Database, localVersions)
             } else {
+                // Step 4b - if Data is Empty, then show UiState.Error() toast message
                 uiState.value =
                     UiState.Error(DataSource.Database, "Database empty!")
             }
 
+            // Step 5 - In any case, Show Loading Indicator for LoadFromNetwork
             uiState.value = UiState.Loading.LoadFromNetwork
 
+            // Step 6 - If Network Response is Successful, then show UiState.Success(networkData)
             try {
                 uiState.value = UiState.Success(
                     DataSource.Network,
