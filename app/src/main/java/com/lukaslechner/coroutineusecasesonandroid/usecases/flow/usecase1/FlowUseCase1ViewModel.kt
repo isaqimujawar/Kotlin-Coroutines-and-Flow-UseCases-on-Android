4221997c36dfe43f5a0dec8f050952f3376ca927
M Isaq <maddy829019@gmail.com>
2024-02-24 17:27:01 +0530

    Section 13 - Displaying the stock list on the Screen

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
@@ -1,12 +1,22 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase1
 
-import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import kotlinx.coroutines.launch
 
 class FlowUseCase1ViewModel(
     stockPriceDataSource: StockPriceDataSource
 ) : BaseViewModel<UiState>() {
 
-    val currentStockPriceAsLiveData: LiveData<UiState> = TODO()
+    val currentStockPriceAsLiveData: MutableLiveData<UiState> = MutableLiveData()
+
+    init {
+        viewModelScope.launch {
+            stockPriceDataSource.latestStockList.collect { stockList ->
+                currentStockPriceAsLiveData.value = UiState.Success(stockList)
+            }
+        }
+    }
 
 }
