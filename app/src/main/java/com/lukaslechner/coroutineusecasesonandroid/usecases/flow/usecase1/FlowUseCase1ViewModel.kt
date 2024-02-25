a9a0f3319cd65e442d08bd43c408309293626ea8
M Isaq <maddy829019@gmail.com>
2024-02-25 12:36:08 +0530

    Section 13 - lifecycle operators onStart() and onCompletion()

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
@@ -4,7 +4,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import timber.log.Timber
 
 class FlowUseCase1ViewModel(
     stockPriceDataSource: StockPriceDataSource
@@ -15,8 +18,18 @@ class FlowUseCase1ViewModel(
     init {
         stockPriceDataSource
             .latestStockList
+            .onStart {
+                Timber.d("The Flow starts to be collected 1")
+            }
+            .onCompletion { cause ->
+                Timber.d("Flow has completed")
+                Timber.d("cause is ${cause?.message}")
+            }
             .onEach { stockList ->
                 currentStockPriceAsLiveData.value = UiState.Success(stockList)
+            }
+            .onStart {
+                Timber.d("The Flow starts to be collected 2")
             }
             .launchIn(viewModelScope)
     }
