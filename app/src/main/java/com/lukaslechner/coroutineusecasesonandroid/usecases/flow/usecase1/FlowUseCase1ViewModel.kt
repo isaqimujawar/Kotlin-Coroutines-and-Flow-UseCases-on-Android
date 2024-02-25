cbd6f3651c57aec9440c31e196a178d4e4923c0d
M Isaq <maddy829019@gmail.com>
2024-02-25 14:09:13 +0530

    Section 13 - Terminal Operator "asLiveData()"

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
@@ -1,41 +1,30 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase1
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import timber.log.Timber
 
 /**
- * Using job to cancel coroutine
- * cancelling the coroutine will stop the Flow request of new stock prices
+ * Using Terminal Operator "asLiveData()" made our code concise and more readable.
  */
 class FlowUseCase1ViewModel(
-    private val stockPriceDataSource: StockPriceDataSource
+    stockPriceDataSource: StockPriceDataSource
 ) : BaseViewModel<UiState>() {
 
-    val currentStockPriceAsLiveData: MutableLiveData<UiState> = MutableLiveData()
-
-    var job: Job? = null
-
-    fun startFlowCollection() {
-        job = stockPriceDataSource
+    val currentStockPriceAsLiveData: LiveData<UiState> = stockPriceDataSource
             .latestStockList
-            .map { stockList -> UiState.Success(stockList) as UiState }
-            .onStart { emit(UiState.Loading) }
-            .onEach { uiState -> currentStockPriceAsLiveData.value = uiState }
+            .map { stockList ->
+                UiState.Success(stockList) as UiState
+            }
+            .onStart {
+                emit(UiState.Loading)
+            }
             .onCompletion {
                 Timber.tag("Flow").d("Flow has completed. Cause is ${it?.message}")
             }
-            .launchIn(viewModelScope)
-    }
-
-    fun stopFlowCollection() {
-        job?.cancel()
-    }
+            .asLiveData()
 }
