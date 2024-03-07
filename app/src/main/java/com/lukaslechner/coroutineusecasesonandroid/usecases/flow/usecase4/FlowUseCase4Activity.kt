7f86eafa6875c6348e3181e3dd45ecdea9d44b41
M Isaq <maddy829019@gmail.com>
2024-03-07 14:23:57 +0530

    Section 15: Using repeatOnLifecycle() to make the flow collection lifecycle-aware

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase4/FlowUseCase4Activity.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase4/FlowUseCase4Activity.kt
@@ -2,14 +2,15 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase4
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseActivity
 import com.lukaslechner.coroutineusecasesonandroid.base.flowUseCase4Description
 import com.lukaslechner.coroutineusecasesonandroid.databinding.ActivityFlowUsecase1Binding
 import com.lukaslechner.coroutineusecasesonandroid.utils.setGone
 import com.lukaslechner.coroutineusecasesonandroid.utils.setVisible
 import com.lukaslechner.coroutineusecasesonandroid.utils.toast
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import org.joda.time.LocalDateTime
 import org.joda.time.format.DateTimeFormat
@@ -23,28 +24,25 @@ class FlowUseCase4Activity : BaseActivity() {
         ViewModelFactory(NetworkStockPriceDataSource(mockApi(applicationContext)))
     }
 
-    private var job: Job? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.recyclerView.adapter = adapter
-    }
 
-    override fun onStart() {
-        super.onStart()
+        /*
+        // Using flowWithLifecycle() to make the flow collection lifecycle-aware
+        lifecycleScope.launch {
+                viewModel.currentStockPriceAsFlow
+                    .flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+                    .collect { uiState -> render(uiState) }
+        }*/
 
-        job = lifecycleScope.launch {
-            viewModel.currentStockPriceAsFlow.collect { uiState ->
-                render(uiState)
+        // Using repeatOnLifecycle() to make the flow collection lifecycle-aware
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.currentStockPriceAsFlow.collect { uiState -> render(uiState) }
             }
         }
-    }
-
-    override fun onStop() {
-        super.onStop()
-
-        job?.cancel()
     }
 
     private fun render(uiState: UiState) {
