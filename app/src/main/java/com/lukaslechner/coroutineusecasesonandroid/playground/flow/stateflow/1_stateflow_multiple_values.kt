b30b3685cd966fa3de84b74f4b1d4dc78c85ec70
M Isaq <maddy829019@gmail.com>
2024-03-08 01:00:27 +0530

    Section 15: StateFlow

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/flow/stateflow/1_stateflow_multiple_values.kt
@@ -0,0 +1,28 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.flow.stateflow
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+fun main() {
+    val scope = CoroutineScope(Dispatchers.Default)
+
+    val counter = MutableStateFlow(99)
+
+    scope.launch {
+        repeat(5) {
+            counter.emit(it)
+            delay(200)
+        }
+    }
+
+    scope.launch {
+        counter.collect {
+            println("Collected $it")
+        }
+    }
+
+    Thread.sleep(600)
+}
